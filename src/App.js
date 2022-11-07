418927fc5fbb8e93fa64b529b4d905f0c50ee1e3
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-11-07 11:35:48 -0600

    set up fetch for occupations and states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react'
+import SignUpForm from './components/SignUpForm'
 
-import './App.css';
 
 function App() {
+  const [data, setData] = useState()
+
+
+  useEffect(() => {
+    async function fetchStatesAndOccupations(){
+      let response = await fetch("https://frontend-take-home.fetchrewards.com/form")
+      const resData = await response.json()
+      setData(resData)
+    }
+    fetchStatesAndOccupations()
+  }, [])
+
   return (
-    <div className="App">
-      Hi
-    </div>
+    <>
+    <SignUpForm occupations={data.occupations} states={data.states}/>
+    </>
   );
 }
 
